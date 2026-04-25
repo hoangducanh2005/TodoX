954bc975796c75236be9031731e53e6084a4ec53
hoangducanh2005 <anh.hd235640@sis.hust.edu.vn>
2026-04-25 10:12:26 +0700

    Phase 2 : Link API to backend

--- backend/src/controllers/tasksControllers.js
+++ backend/src/controllers/tasksControllers.js
@@ -1,9 +1,28 @@
-export const getAllTasks = (request, response) => {
-    response.status(200).send({message: "You have 36 tasks to do!"});
+import Task from '../models/Tasks.js';
+
+
+export const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find();    // lay tat ca data tu database
+        res.status(200).json(tasks);    //tra data duoiu dang json ve client
+
+    } catch (error) {
+        console.error('Error when calling getAllTasks:', error);
+        res.status(500).json({ message: 'System Error' });
+    }
 };
 
-export const createTask = (req, res) => {
-    res.status(200).json({ message: 'Task created successfully!' });
+export const createTask = async (req, res) => {
+    try {
+        const { title } = req.body;
+        const task = new Task({ title });
+        
+        const newTask = await task.save();
+        res.status(201).json(newTask);
+    } catch (error) {
+        console.error('Error when calling createTask:', error);
+        res.status(500).json({ message: 'System Error' });
+    }
 };
 
 export const updateTask = (req, res) => {
--- backend/src/server.js
+++ backend/src/server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5001; // Use the PORT from environment variable
 const app = express();
 
 connectDB();  // Connect to MongoDB
+app.use(express.json()); // Middleware to parse JSON request bodies
 
 app.use("/api/tasks", taskRoutes);
 
